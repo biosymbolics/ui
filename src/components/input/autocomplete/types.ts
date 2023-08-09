1b3f36cf6ad61abda43d9acd787dfab004d9fb80
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-08-08 21:34:56 -0600

    slider and other adjustments

--- src/components/input/autocomplete/types.ts
+++ src/components/input/autocomplete/types.ts
@@ -1,39 +1,40 @@
-import { AutocompleteProps as _JoyAutocompleteProps } from "@mui/joy/Autocomplete";
+import { AutocompleteProps as _JoyAutocompleteProps } from '@mui/joy/Autocomplete'
 
-import { AllOrNothing } from "@/types/helpers";
+import { AllOrNothing } from '@/types/helpers'
 
-export type BaseOption = string | Record<string, any>;
+export type BaseOption = string | Record<string, any>
 
 type JoyAutocompleteProps<
-  T extends BaseOption,
-  Multiple extends boolean | undefined,
-> = _JoyAutocompleteProps<T, Multiple, false, false>;
+    T extends BaseOption,
+    Multiple extends boolean | undefined,
+> = _JoyAutocompleteProps<T, Multiple, false, false>
 
 type IsCreatableProps<T> = AllOrNothing<{
-  isCreatable: true;
-  optionIdField: keyof T;
-  optionLabelField: keyof T;
-}>;
+    isCreatable: true
+    optionIdField: keyof T
+    optionLabelField: keyof T
+}>
 
 export type AutocompleteProps<
-  T extends BaseOption,
-  Multiple extends boolean | undefined,
+    T extends BaseOption,
+    Multiple extends boolean | undefined,
 > = {
-  defaultValue?: JoyAutocompleteProps<T, Multiple>["defaultValue"];
-  id?: string;
-  /**
-   * if true, the user can create new options
-   * (corresponds to the `freeSolo` prop of `Autocomplete`)
-   */
-  isCreatable?: boolean;
-  isLoading?: JoyAutocompleteProps<T, Multiple>["loading"];
-  options: JoyAutocompleteProps<T, Multiple>["options"];
-  /**
-   * label/title for the autocomplete box
-   */
-  label?: string;
-  placeholder?: JoyAutocompleteProps<T, Multiple>["placeholder"];
-  variant?: JoyAutocompleteProps<T, Multiple>["variant"];
+    defaultValue?: JoyAutocompleteProps<T, Multiple>['defaultValue']
+    error?: JoyAutocompleteProps<T, Multiple>['error']
+    id?: string
+    /**
+     * if true, the user can create new options
+     * (corresponds to the `freeSolo` prop of `Autocomplete`)
+     */
+    isCreatable?: boolean
+    isLoading?: JoyAutocompleteProps<T, Multiple>['loading']
+    options: JoyAutocompleteProps<T, Multiple>['options']
+    /**
+     * label/title for the autocomplete box
+     */
+    label?: string
+    placeholder?: JoyAutocompleteProps<T, Multiple>['placeholder']
+    variant?: JoyAutocompleteProps<T, Multiple>['variant']
 } & {
-  helperText?: string;
-} & IsCreatableProps<T>;
+    helperText?: string
+} & IsCreatableProps<T>
---
+++ src/components/input/slider.tsx
@@ -0,0 +1,38 @@
+import { ReactElement } from 'react'
+import {
+    default as JoySlider,
+    SliderProps as JoySliderProps,
+} from '@mui/joy/Slider'
+import FormLabel from '@mui/joy/FormLabel'
+import FormControl from '@mui/joy/FormControl'
+import { getSelectableId } from '@/utils/string'
+
+type SliderProps = {
+    defaultValue?: JoySliderProps['defaultValue']
+    disabled?: JoySliderProps['disabled']
+    error?: boolean
+    id?: JoySliderProps['id']
+    label?: string
+    orientation?: JoySliderProps['orientation']
+    marks?: JoySliderProps['marks']
+    min?: JoySliderProps['min']
+    max?: JoySliderProps['max']
+    size?: JoySliderProps['size']
+    step?: JoySliderProps['step']
+    valueLabelDisplay?: JoySliderProps['valueLabelDisplay']
+}
+
+export const Slider = ({
+    error,
+    id,
+    label,
+    ...props
+}: SliderProps): ReactElement<SliderProps> => {
+    const form_id = id || getSelectableId(label)
+    return (
+        <FormControl id={form_id} error={error}>
+            {label && <FormLabel>{label}</FormLabel>}
+            <JoySlider {...props} />
+        </FormControl>
+    )
+}
