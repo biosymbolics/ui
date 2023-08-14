afec4fa960a634f1605407d674532e0cb0558aea
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-08-14 15:58:50 -0600

    cleanup

--- src/app/core/patents/patents.tsx
+++ src/app/core/patents/patents.tsx
@@ -1,7 +1,6 @@
 'use server';
 
 import { cache } from 'react';
-import { GridColDef } from '@mui/x-data-grid';
 import Box from '@mui/joy/Box';
 import 'server-only';
 
@@ -11,14 +10,7 @@ import { Patent, PatentResponse, PatentSearchArgs } from '@/types/patents';
 import { getFetchOptions } from '@/utils/actions';
 import { getQueryArgs } from '@/utils/patents';
 
-import {
-    DetailContent,
-    formatDate,
-    formatNumber,
-    getPatentYearsClass,
-    getScoresClass,
-    getStyles,
-} from './client';
+import { DetailContent, getPatentColumns, getStyles } from './client';
 
 const fetchPatents = cache(
     async (args: PatentSearchArgs): Promise<Patent[]> => {
@@ -34,50 +26,14 @@ const fetchPatents = cache(
     }
 );
 
-const PATENT_COLUMNS: GridColDef[] = [
-    { field: 'publication_number', headerName: 'Pub #', width: 160 },
-    { field: 'title', headerName: 'Title', width: 500 },
-    {
-        field: 'patent_years',
-        headerName: 'Yrs Left',
-        width: 75,
-        description: 'Patent years remaining.',
-        cellClassName: getPatentYearsClass,
-    },
-    {
-        field: 'score',
-        headerName: 'Suitability',
-        width: 85,
-        valueFormatter: formatNumber,
-        cellClassName: getScoresClass,
-        description:
-            'Suitability of patent, in terms of patent type (CoM vs MoU), patented thing (compound > device) and patent years remaining.',
-    },
-    {
-        field: 'search_rank',
-        headerName: 'Relevancy',
-        width: 85,
-        valueFormatter: formatNumber,
-        cellClassName: getScoresClass,
-        description: 'Relevancy of patent to search terms.',
-    },
-    {
-        field: 'priority_date',
-        headerName: 'Priority Date',
-        width: 200,
-        valueFormatter: formatDate,
-    },
-    { field: 'assignees', headerName: 'Assignees', width: 250 },
-    { field: 'attributes', headerName: 'Attributes', width: 100 },
-];
-
 export const Patents = async (args: PatentSearchArgs) => {
+    const columns = getPatentColumns();
     try {
         const patents = await fetchPatents(args);
         return (
             <Box sx={getStyles}>
                 <DataGrid
-                    columns={PATENT_COLUMNS}
+                    columns={columns}
                     detailComponent={DetailContent<Patent>}
                     // pinned column === column title rendering issues
                     // initialState={{
