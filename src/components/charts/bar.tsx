ea2bdf0336a2bfa55e5e54d1fe2b7397b10a6b23
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-08-14 18:31:50 -0600

    type safety on clicks; relevancy default

--- src/components/charts/bar.tsx
+++ src/components/charts/bar.tsx
@@ -2,7 +2,9 @@
 
 import { usePathname } from 'next/navigation';
 import Chart from 'react-apexcharts';
+import get from 'lodash/fp/get';
 import isEmpty from 'lodash/fp/isEmpty';
+import isNumber from 'lodash/fp/isNumber';
 
 import { useNavigation } from '@/hooks/navigation';
 
@@ -20,20 +22,29 @@ export const Bar = ({
     series,
     ...props
 }: BarChartProps): JSX.Element | null => {
+    const pathname = usePathname();
+    const { navigate } = useNavigation();
+
     if (isEmpty(series)) {
         return <span>No data</span>;
     }
 
-    const pathname = usePathname();
-    const { navigate } = useNavigation();
-
     const options: ChartOptions = {
         chart: {
             events: {
-                dataPointSelection: (event, chartContext, config) => {
-                    navigate(
-                        `${pathname}?terms=${labels[config.dataPointIndex]}`
-                    );
+                dataPointSelection: (
+                    event,
+                    chartContext,
+                    config: Record<string, unknown>
+                ) => {
+                    const idx = get('dataPointIndex', config);
+                    const term = idx && isNumber(idx) ? labels[idx] : null;
+
+                    if (!term) {
+                        console.warn("Couldn't find term for index", idx);
+                        return;
+                    }
+                    navigate(`${pathname}?terms=${term}`);
                 },
             },
         },
