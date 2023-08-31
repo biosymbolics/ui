a7665942f1ec966229a7851cf94a2e11e0711263
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-08-31 17:43:44 -0600

    fixed build errors

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,9 +1,15 @@
 'use client';
 
-import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
+import {
+    SyntaxHighlighterProps,
+    LightAsync as SyntaxHighlighter,
+} from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-export const Code = ({ children, ...props }: ReturnType<SyntaxHighlighter>) => (
+export const Code = ({
+    children,
+    ...props
+}: SyntaxHighlighterProps): JSX.Element => (
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     <SyntaxHighlighter language="json" style={dark} {...props}>
         {children}
