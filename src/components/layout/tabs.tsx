2cd2a85835a82b7f36746300e44f3d5e23ec778d
Kristin Lindquist <lindquist.kristin@gmail.com>
2024-01-28 11:39:18 -0700

    default tab

--- src/components/layout/tabs.tsx
+++ src/components/layout/tabs.tsx
@@ -5,6 +5,7 @@ import Tab from '@mui/joy/Tab';
 import TabList from '@mui/joy/TabList';
 import TabPanel from '@mui/joy/TabPanel';
 import JoyTabs from '@mui/joy/Tabs';
+import isEmpty from 'lodash/fp/isEmpty';
 
 import { useNavigation } from '@/hooks/navigation';
 
@@ -26,8 +27,16 @@ export const Tabs = ({
     tabs: TabDef[];
 }): JSX.Element => {
     const { setParam } = useNavigation();
+    if (isEmpty(tabs)) {
+        console.error('Empty tabs');
+        return <span />;
+    }
+
     return (
-        <JoyTabs onChange={(e, id) => setParam('tab', `${id}`)} value={openId}>
+        <JoyTabs
+            onChange={(e, id) => setParam('tab', `${id}`)}
+            value={openId || tabs[0].id}
+        >
             <TabList>
                 {tabs.map(({ id, label }) => (
                     <Tab key={`${id}-tab`} value={id} variant="soft">
