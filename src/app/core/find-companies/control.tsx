dbd8bc34ab27b90659f03e1486983a9896e1fc37
Kristin Lindquist <lindquist.kristin@gmail.com>
2024-02-15 16:59:51 -0700

    removing gpt expansion

--- src/app/core/find-companies/control.tsx
+++ src/app/core/find-companies/control.tsx
@@ -5,21 +5,17 @@ import Grid from '@mui/joy/Grid';
 import Typography from '@mui/joy/Typography';
 
 import { Section } from '@/components/layout/section';
-import { Button, Checkbox, Slider, TextArea } from '@/components/input';
+import { Button, Slider, TextArea } from '@/components/input';
 import { FindCompaniesParams } from '@/types';
 import { useNavigation } from '@/hooks/navigation';
 
 export const FindCompaniesControl = ({
     description: initialDescription = '',
     children,
-    useGptExpansion: initialUseGptExpansion = true,
     k: initialK = 1000,
 }: FindCompaniesParams & { children: ReactNode }) => {
     const { setParams } = useNavigation();
     const [description, setDescription] = useState<string>(initialDescription);
-    const [useGptExpansion, setUseGptExpansion] = useState<boolean>(
-        initialUseGptExpansion
-    );
     const [k, setK] = useState<number>(initialK);
     const [isPending] = useTransition();
 
@@ -28,7 +24,7 @@ export const FindCompaniesControl = ({
             <Section variant="separated">
                 <Section variant="l2">
                     <Typography gutterBottom level="h4">
-                        Invention Description
+                        Describe technology, platform, or theme
                     </Typography>
                     <TextArea
                         aria-label="description"
@@ -44,24 +40,14 @@ export const FindCompaniesControl = ({
                         <Grid xs={12} sm={4}>
                             <Slider<number>
                                 defaultValue={k}
-                                label="Nearest Neighbors"
+                                label="Search Breadth"
                                 onChange={(newK) => setK(newK)}
                                 min={50}
                                 max={5000}
                                 size="lg"
                                 step={100}
                                 sx={{ mr: 3 }}
-                                tooltip="How distantly to search for related IP. Higher values will take longer to compute."
-                            />
-                        </Grid>
-                        <Grid>
-                            <Checkbox
-                                checked={useGptExpansion}
-                                onChange={(e) =>
-                                    setUseGptExpansion(e.target.checked)
-                                }
-                                label="Expand with GPT?"
-                                tooltip="Use GPT to expand to generate a description from a small snippet of text. Mostly for testing. This will result in a 10-30 more seconds of waiting."
+                                tooltip="How distantly to search for related IP (KNN). Higher values will take longer to compute."
                             />
                         </Grid>
                     </Grid>
@@ -74,7 +60,6 @@ export const FindCompaniesControl = ({
                             setParams(
                                 {
                                     description,
-                                    useGptExpansion,
                                     k,
                                 },
                                 true
